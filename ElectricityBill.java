490b0da78c91f5801d98e1fcab9dd3f92d794e34
Tarun Sehgal <tarunsehgal27@gmail.com>
2020-05-30 12:24:42 +0530

    Create ElectricityBill.java

---
+++ ElectricityBill.java
@@ -0,0 +1,45 @@
+// Java implementation to calculate the 
+// electricity bill 
+  
+import java.util.*; 
+  
+class ElectricityBill { 
+  
+    // Function to calculate the 
+    // electricity bill 
+    public static int calculateBill(int units) 
+    { 
+  
+        if (units <= 100) { 
+            return units * 10; 
+        } 
+        else if (units <= 200) { 
+            return (100 * 10) 
+                + (units - 100) 
+                      * 15; 
+        } 
+        else if (units <= 300) { 
+            return (100 * 10) 
+                + (100 * 15) 
+                + (units - 200) 
+                      * 20; 
+        } 
+        else if (units > 300) { 
+            return (100 * 10) 
+                + (100 * 15) 
+                + (100 * 20) 
+                + (units - 300) 
+                      * 25; 
+        } 
+        return 0; 
+    } 
+  
+    // Driver Code 
+    public static void main(String args[]) 
+    { 
+        int units = 250; 
+  
+        System.out.println( 
+            calculateBill(units)); 
+    } 
+} 
